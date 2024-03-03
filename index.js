f760ccf4a5b912fe384a48d7590529469127fd07
beenow011 <abhinavnb11@gmail.com>
2024-03-03 20:15:12 +0530

    int

--- index.js
+++ index.js
@@ -3,12 +3,10 @@
 
 
 import dotenv from "dotenv"
-import connectDB from "./db/index.js";
-import { app } from "./app.js";
+import connectDB from "./src/db/index.js";
+import { app } from "./src/app.js";
 
-dotenv.config({
-    path: './env'
-})
+dotenv.config()
 
 connectDB().then(() => app.listen(process.env.PORT || 8000, () => {
     console.log(`Server is running at port : ${process.env.PORT}`)
--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+const uri = `mongodb+srv://abhinavnb11:benaka234@cluster0.vbw5nr9.mongodb.net`
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
+        const connectionInstance = await mongoose.connect(uri)
         console.log(`\n MongoDB connected!! DB  host: ${connectionInstance.connection.host}`)
     } catch (err) {
         console.log("MONGODB connection error", err);
