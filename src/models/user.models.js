1ddd85b76d604af678374c791a47f3e74866b80e
beenow011 <abhinavnb11@gmail.com>
2024-04-17 17:41:23 +0530

    reset accessToken

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -61,15 +61,13 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 }
 
 userSchema.methods.generateAccessToken = function () {
-    return Jwt.sign({
-        exp: Math.floor(Date.now() / 1000) + (10 * 60),
-        data: {
+    return Jwt.sign(
+        {
             _id: this._id,
             email: this.email,
             username: this.username,
             fullName: this.fullName,
-        }
-    },
+        },
         process.env.ACCESS_TOKEN_SECRET,
         {
             expiresIn: '10h'
